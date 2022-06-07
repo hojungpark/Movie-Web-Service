ffbfb9044b9fa5fc4e5a411ac7abf51868f53601
hojungpark-sfu <tacopak21@gmail.com>
2022-06-07 16:12:47 -0700

    update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,16 +1,38 @@
 import { useEffect, useState } from "react";
-
+import Movie from "../components/Movie";
 function Home() {
   const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    fetch(
+  const [loading, setLoading] = useState(true);
+  const getMovies = async () => {
+    const reponse = await fetch(
       `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    )
-      .then((response) => response.json)
-      .then((json) => setMovies(json.data.movies));
+    );
+    const json = await reponse.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
   }, []);
-  console.log(movies.title);
-  return <div>Working</div>;
+  return (
+    <div>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Home;
