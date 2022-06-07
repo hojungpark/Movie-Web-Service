7bcf5ae1e0d755081ff172b240d3c48a27eae46f
hojungpark-sfu <tacopak21@gmail.com>
2022-06-07 14:56:17 -0700

    genres added to each movie

--- src/App.js
+++ src/App.js
@@ -27,6 +27,12 @@ function App() {
             <div key={movie.id}>
               <img src={movie.medium_cover_image} />
               <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
             </div>
           ))}
         </div>
