f7f88a171e7337aa554736a5c0dbd89fb5b0ceed
hojungpark-sfu <tacopak21@gmail.com>
2022-06-08 12:28:14 -0700

    delete link component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 import styles from "./Movie.module.css";
 
 function Movie({ id, coverImg, title, year, summary, genres }) {
